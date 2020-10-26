2233a09bea7c2513b6393de69a5a6645e2270e68
Anna Buckles <willib08@gmail.com>
2020-10-26 16:18:55 +0200

    update

---
+++ Week2/script.js
@@ -0,0 +1,153 @@
+
+//Exercise 1
+
+    //console.log5 + "34" =====534
+    //console.log(5 - "4"); =====1
+    //console.log(10 % 5); =====0
+    //console.log(5 % 10); =====5
+    //console.log("Java" + "Script"); =====Javascript
+    //console.log(" " + " "); ===== space space
+    //console.log(" " + 0); ===== space 0
+    //console.log(true + true); ===== 2
+    //console.log(true + false); =====1
+    //console.log(false + true); =====1
+    //console.log(false - true); ===== -1
+    //console.log(3 - 4); ===== -1
+    //console.log("Bob" - "bill"); ===== NaN
+
+
+//Exercise2
+
+    //let me =["my", "favorite", "color", "is", "blue"];
+    //let sentence = me.join (" ");
+    //console.log(sentence);
+
+ //Exercise3
+
+   //let str1 = 'mix';
+   //let str2 = 'pod';
+   //let newword= str2.slice(0,2) + str1.slice(2,3) + " " + str1.slice(0,2) + str2.slice(2,3);
+   //console.log(newword);
+   //let newWord should be equal to 'pod mix'
+
+//Exercise4  
+
+  //let nr1 = (prompt('number'))
+  //let nr2 = (prompt('number'))
+  //let sum = alert(nr1 * nr2)
+  //console.log(sum)
+
+
+//Exercise5   !this one does not work! FIND THE SOLUTION!
+
+  //You’re given a string of words. You need to find the word “Nemo”, and return a string like this: “I found Nemo at [the order of the word you find nemo]!”.
+  //Bonus : If you can’t find Nemo, console.log “I can’t find Nemo :(“.
+  //Hint : use an if/else statement
+  //Examples
+  //"I am finding Nemo !" ➞ "I found Nemo at 4!"
+ //var sentence= "I am finding Nemo!"
+ //var position = sentence.search("nemo")
+ //var nemo= sentence.split(" ")
+ //var position= nemo.indexof("nemo")
+ //console.log("I found Nemo at " + position)
+
+
+ //Exercise6 Ninja1
+    //console.log(5 >= 1) ===== true
+    //console.log(0 === 1) ===== false
+    //console.log(4 <= 1) ===== false
+    //console.log(1 != 1) =====false
+    //console.log("A" > "B") ===== false
+    //console.log("B" < "C") ===== true
+    //console.log("a" > "A") ===== true
+    //console.log("b" < "A") ===== false
+    //console.log(true === false) ===== false
+    //console.log(true != true) ===== false
+
+//Exercise7 Ninja2    
+    //let c;
+    //let a = 34;
+    //let b = 21;
+    //a = 2;
+    //a + b;
+    //console.log(a+b)  result : 23
+    //console.log(c)   result: undefined
+    //console.log(3 + 4 + '5'); ===== 75
+
+//Exercise8 Ninja3
+    //Ask the user for a string of numbers separated by
+    //a comma and space, 
+    //return the product of the numbers.
+    //Hint: use some string methods
+    //"2, 3"➞ 6
+    //var numbers = prompt ("pick two numbers to add using a comma and space");
+    //var slice = numbers.split(", ");
+    //console.log(slice[0]*slice[1]);
+
+//Exercise9 Ninja4
+    //Hint: if statement (tomorrow’s lesson)
+    //Ask the user for a number, 
+    //return a string of the word “Boom”, which varies in the following ways:
+
+    //The string should include n number of “o”s, unless n is below 2 (in that case, return “boom”).
+    //If n is evenly divisible by 2, add an exclamation mark to the end.
+    //If n is evenly divisible by 5, return the string in ALL CAPS.
+    //If n is evenly divisible by both 2 and 5, 
+    //return the string in ALL CAPS and add an exclamation mark to the end.
+    //The example below should help clarify these instructions.
+    //Examples
+    //4  ➞ "Boooom!"
+    // There are 4 "o"s and 4 is divisible by 2 (exclamation mark included)
+    //1 ➞ "boom"
+    // 1 is lower than 2, so we return "boom" 
+
+     //var num= prompt("pick a number")
+     //var boom;
+     //if(num>2){
+    //boom="b"+"o".repeat(num)+"m" 
+    //}
+    //else{
+    //boom="boom"
+    //}
+    //if(num % 2 == 0){
+    //boom=boom+"!"
+    //}
+    //else if (num % 5 == 0){
+    //boom=boom.toUpperCase()
+    //}
+   //console.log (boom)
+
+
+//Daily Challenge
+//1. remove Banana from the array
+//let fruit = ["Banana", "Apples", "Oranges", "Blueberries"];      YAY!
+//let newfruit = fruit.shift(0);
+//console.log(fruit)
+
+//2.Sort the array in order
+//let fruit = ["Banana", "Apples", "Oranges", "Blueberries"];      YAY!
+//console.log(fruit[1], fruit[0], fruit[3],fruit[2]);
+
+//3. Put kiwi at the end of the array.
+//let fruit = ["Banana", "Apples", "Oranges", "Blueberries"];      YAY!
+//fruit.push("kiwi");
+//console.log(fruit);
+
+//4.Remove Apples from the array. Don't use the same method as in Q1.   FAIL
+//let fruit = ["Banana", "Apples", "Oranges", "Blueberries"];
+//let favorite = fruit.slice(0,2);
+//console.log(favorite);
+
+//5a.Sort the array in reverse order.
+//let fruit = ["Banana", "Apples", "Oranges", "Blueberries"];     YAY!
+//let newfruit = fruit.reverse();
+//console.log(fruit);
+
+//5b.Access the item “Oranges”.
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]] 
+let newfruit = moreFruits.slice (2)
+console.log(newfruit);
+
+
+
+
---
+++ script.js
@@ -0,0 +1,205 @@
+// Write as comments the steps of the resolution of this piece of code
+
+// Guess what will be the result before checking it
+
+
+
+// let a = 1+ 2;  //4
+
+// switch (a) {
+// case 3:
+// alert( 'Too small' );
+// break;
+// case 4:
+// break;
+// case 5:
+// alert( 'Too large' );
+//  break;
+// default:
+// alert( "I don't know such values" );
+// }
+
+
+
+//Create a stuctured html file linked to a JS file
+
+//1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+//2. Create an array which contains the object you have made above and name the array "database".
+
+//3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+
+// let person = {
+
+//   username: 'Buckles',
+//   password: 'notyourbusiness'
+// }
+
+// //console.log(person);
+
+// var database = [person];
+
+// //console.log(database)
+
+// var newsfeed = [];
+
+// let isaac = {
+//    username:'isaac1',
+//    timeline: 'Coding with Javascript'
+// }
+// let shai = {
+//    username:'shaico12',
+//    timeline: 'teacher\'s pet'
+// }
+// let sheryl = {
+   
+//    username:'sheryledery55',
+//    timeline: 'no more corona'
+// }
+// newsfeed.push(isaac, shai, sheryl);
+// console.log(newsfeed);
+
+
+//EXERCISE 1:SIMPLE IF/ELSE STATEMENT
+//1.Create 2 variables, x and y. Each of them has a different numeric value.
+//2.Write an if/else statement that checks the biggest number.
+//If x equals 5 and y equals 2, the program should display: "x is the biggest number"
+
+// let x = 5
+// let y = 2
+// if ( x > y){
+//   console.log ( x + " is bigger than " + y) 
+
+
+//EXERCISE 2:Chihuaha
+//Create a variable newDog that is equal to the string “Chihuahua”.
+//Check and display how many letters are in newDog.
+//Display the newDog variable in uppercase and then in lowercase (no need to create new variables, just use 2 console.log).
+//Check if the variable newDog equals to “Chihuahua”
+//if it does, display ‘I love Chihuahua, it’s my favorite dog breed’
+//else, console.log ‘I dont care, I prefer cats’
+
+// var newDog = "Chihuahua";
+// //console.log(newDog.length);
+// //console.log(newDog.toLocaleUpperCase());
+// //console.log(newDog.toLocaleLowerCase());
+// if (newDog == "Chihuahua"){
+// console.log("I love Chihuahua, its my favorite dog breed");
+// } else {
+//   console.log("I dont care, i prefer cats");
+// }
+
+
+//Exercise 3: Even Or Not Even   !!DO THIS!!!!
+//Ask a number to the user, and save it to a variable.
+//Check if the variable is an even number
+//If it is, display: “x is an even number’. 
+//Where x is the actual number of the user.
+//If it isn’t, display “x is not an even number’. 
+//Where x is the actual number of the user
+
+//var num = prompt("enter a number", 5)
+    //if(num%2==0){
+      //console.log("even")
+          //}
+    //else{
+      //console.log("odd")
+          //}
+
+// Exercise 4: Group Chat  ~THIS DOESNT WORK~
+// let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+// Using the array above, console.log the number of users 
+// in a group conversation based on the following rules:
+// If there is no one, display “no one is online”.
+// If there is 1 person, display “<name_user> is online”.
+// If there are 2 people, display “<name_user1> and <name_user2> 
+// are online”.
+// If there are n>2 people, display the first two names and 
+// add “and n-2 more are online”.
+// For example, if there are 5 users, it should display:
+
+// name_user1, name_user2 and 3 more are online
+
+ // let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+ //   if (users.length === 0){
+ //    console.log("no one is online");
+ // }
+ // else if(users.length === 1){
+ //    console.log(users[0]+" is online");
+ // }
+ // else if(users.length === 2){
+ //     console.log(users[0]+ " and " + users[1] +" are online");
+ // }
+ // else if(users.length > 2){
+ //     console.log(users[0]+ " , ” + users[1]+ " and " + (users.length -2)+ "are online);
+ // }
+
+
+//EXERCISE 1 gold: THE WORLD TRANSLATOR
+
+// Ask the user which language he speaks.
+// Create a few conditions :
+// If he speaks French : display “Bonjour”
+// If he speaks English : display “Hello”
+// If he speaks Hebrew : display “Shalom”
+// If he doesn’t speak any of these 3 
+// languages: display ‘01110011 01101111 01110010 01110010 01111001’
+
+// let language = prompt ("what language do you speak?")
+// switch(language){
+//   case "french": console.log("Bonjour");
+//   break;
+//   case "english": console.log("Hello");
+//   break;
+//   case "hebrew": console.log("Shalom");
+//   break;
+
+//   default: console.log("01110011 01101111 01110010 01110010 01111001")
+// }
+
+
+//Exercise 2 : The Grade Assigner
+//Ask the user for his grade
+// If the score is bigger than 90, console.log “A”
+// If the score is between 80 and 90 (included), console.log “B”
+// If the score is between 70(included) and 80 (included), console.log “C”
+// If the score is lower than 70, console.log “D”
+
+//  var grade = prompt ("what is your score?")
+//  if (grade > 90){
+//   console.log("A");
+//  }
+// else if (grade > 80 &&  grade <=90){
+//   console.log("B");
+// } else if (grade > 70 && grade <=80){
+//   console.log("C");
+// } else if (grade < 70){
+//   console.log("D");
+// }
+
+// Exercise 3 : Verbing
+// Create a variable, it must be a verb.
+// If the length of this string is at least 3, it should add ‘ing’ to its end, unless it already ends in ‘ing’, in which case it should add ‘ly’ instead.
+// If the string length is less than 3, it should leave it unchanged.
+// Example:
+// The string is : "swim" , your program should console.log : "swimming"
+// The string is : "swimming", your program should console.log : "swimmingly"
+// The string is : "go" your program should console.log : "go"
+
+ var word = "go";
+ if(word.length >= 3){
+   if (word.endsWith("ing")){
+     word = word + "ly"
+   } else {
+     word = word + "ing"
+   }
+ } console.log(word)
+ alert(word);
+
+
+
+
+
+
+
